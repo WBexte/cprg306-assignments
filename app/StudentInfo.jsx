dcb5b2af75fd668057d257f57f5cb82f6abfca85
William <119466101+WBexte@users.noreply.github.com>
2023-09-14 16:14:52 -0600

    Removed default templating

---
+++ app/StudentInfo.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    <div>
+       <p>Name: William Bexte</p>
+       <p>Course Section: CPRG 306 B</p>
+       <link href=""></link>
+    </div>
+}
